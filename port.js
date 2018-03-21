080aaa77c2e667dc0204dd02625d7df750029d35
Maximilian Michael Schaumann <maximilian.schaumann@gmail.com>
2018-03-21 16:07:07 +0100

    portrait

--- port.js
+++ port.js
@@ -5,8 +5,16 @@
 		})
 		$(".port").find("div").each(function(){
 			$(this).click(function(){$(".hamclose").parent().fadeIn()})
-			
+			$(this).find("img").attr("src")
 			// $(this).click(function(){console.log($(this))})
+		})
+		$("html").on("keyup", function(_e){
+			// console.log(_e.key)
+			switch (_e.key) {
+				case "Escape":
+					$(".hamclose").trigger("click")
+				break;
+			}
 		})
 	})
 })(jQuery)
