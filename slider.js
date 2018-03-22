001a6977b6d8e9650112c7eeddff2592044c05f7
Maximilian Michael Schaumann <maximilian.schaumann@gmail.com>
2018-03-22 10:49:03 +0100

    comments added

--- slider.js
+++ slider.js
@@ -36,20 +36,21 @@
 				break
 			}
 		})
-		setInterval(function(){slideleft()},10000)
-		$(".h2__1").children().hide()
-		
-		var rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()))
+
+		setInterval(function(){slideleft()},10000)										//Autoslide
+
+		$(".h2__1").children().hide()													//HideAllQuotes
+		var rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()))	//GenerateRandomQuoteIndex
 		console.log(rand)
-		if(document.cookie.split("=").splice(1)==rand){
+		if(document.cookie.split("=").splice(1)==rand){									//If random number equals previously generated rand, throw away
 			rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()))
 			$(".h2__1").children().eq(rand).show()
-			document.cookie="previousrand="+rand+";"
+			document.cookie="previousrand="+rand+";"									//Store new random number in cookie if old cookie matches new rand
 			console.log(rand)
 		}
-		else{
+		else{																			//If random number is unique compared to previous attempt, display quote
 			$(".h2__1").children().eq(rand).show()
-			document.cookie="previousrand="+rand+";"
+			document.cookie="previousrand="+rand+";"									//Store random number in cookie for later use
 		}
 		
 	})
