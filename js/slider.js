95d58d45b0a0867f5722a55262db1f29edefcac9
Maximilian Michael Schaumann <maximilian.schaumann@gmail.com>
2018-04-19 21:41:47 +0200

    restructured code

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,20 @@
 // Make background slide in index.html
 // Try using leftarrow/rightarrow!
 (function($, undefined){
-	$(function(){
+	function quoterand(){$(".h2__1").children().hide()													//HideAllQuotes
+			var rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()+1))	//GenerateRandomQuoteIndex
+			console.log(rand)
+			if(document.cookie.split("=").splice(1)==rand){									//If random number equals previously generated rand, throw away
+				rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()+1))
+				$(".h2__1").children().eq(rand).show()
+				document.cookie="previousrand="+rand+";"									//Store new random number in cookie if old cookie matches new rand
+				console.log(rand)
+			}
+			else{																			//If random number is unique compared to previous attempt, display quote
+				$(".h2__1").children().eq(rand).show()
+				document.cookie="previousrand="+rand+";"									//Store random number in cookie for later use
+			}
+		}
 		function slideleft(){
 				$(".bgwrapper").animate({left: -($(".bgwrapper").find("div").first().width())}, function(){
 						$(".bgwrapper").find("div").last().parent().after().append($(".bgwrapper").find("div").first())
@@ -12,6 +25,9 @@
 			$(".bgwrapper").prepend($(".bgwrapper").find("div").last()).css("left",-($(".bgwrapper").find("div").first().width()))
 			$(".bgwrapper").animate({left: 0})
 		}
+		
+	$(function(){
+		quoterand()
 
 		$(".slider").find("ul>li").each(function(){
 			$(this).click(function(){
@@ -51,22 +67,6 @@
 				break
 			}
 		})
-
-		var int=setInterval(function(){slideleft()},10000)								//Autoslide
-
-		$(".h2__1").children().hide()													//HideAllQuotes
-		var rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()))	//GenerateRandomQuoteIndex
-		console.log(rand)
-		if(document.cookie.split("=").splice(1)==rand){									//If random number equals previously generated rand, throw away
-			rand=Math.floor(Math.random() * ($(".h2__1").children().last().index()))
-			$(".h2__1").children().eq(rand).show()
-			document.cookie="previousrand="+rand+";"									//Store new random number in cookie if old cookie matches new rand
-			console.log(rand)
-		}
-		else{																			//If random number is unique compared to previous attempt, display quote
-			$(".h2__1").children().eq(rand).show()
-			document.cookie="previousrand="+rand+";"									//Store random number in cookie for later use
-		}
-		
+		var int=setInterval(function(){slideleft()},10000)								//Autoslide	
 	})
 })(jQuery)
