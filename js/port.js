a318409a3fb004aa6a6a0423f78893d753dfead3
Maximilian Michael Schaumann <maximilian.schaumann@gmail.com>
2018-05-02 00:02:31 +0200

    portfix test

--- js/port.js
+++ js/port.js
@@ -1,6 +1,29 @@
 //used to build "overlay" in 3.html with correct images
 (function($, undefined){
 	$(function(){
+		// function hamslidein(){																				//SlideIn function for easy calling
+		// 	let img="<img src='assets/ic_close_black_1024px.svg'></img>"
+		// 	$(".hampost").show().animate({left:0},300)
+		// 	// $(".hampre").append(img).css("background-image", "none").find("img").addClass("hamclose") 		//sloppy attempt to replace hamburger ico with close ico
+		// 	$(".hampre").toggleClass("active")
+
+		// 	$(".hampost").find("a").each(function(){														//Dynamically set "curr Page" dot by comparing file name with pathname
+		// 		if($(this).attr("href")==location.pathname.split('/').slice(-1)[0]){
+		// 			$(this).addClass("curr")
+		// 		}
+		// 		else if(location.pathname.split('/').slice(-1)[0]===""){
+		// 			$(".hampost").find("a").first().addClass("curr")
+		// 		}
+		// 	})
+		// }
+		// function hamslideoff(){																				//SlideOut function for easy calling
+		// 	$(".hampost").animate({left: "-100%"}, 300,function(){$(this).hide()})
+		// 		// $(".hampre").find("img").remove()
+		// 		// $(".hampre").removeAttr("style")
+		// 		$(".hampre").removeClass("active")
+		// }
+		$.getScript("./ham.js")
+
 		$(".hamclose").click(function(){									//remove .stage children
 			$(this).parent().fadeOut()
 			$(".stage").children().remove()
@@ -34,6 +57,7 @@
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><a class=\"button\">Architecture</a><a class=\"button\">Scenery</a><a class=\"button\">Skyline</a></p>"
 		$(".port").find("div").each(function(){								//find correct image and mount in .stage
 			$(this).click(function(){
+				$("html").off("keydown")
 				$(".hamclose").parent().fadeIn()
 				$(".stage").append("<img>").find("img").attr("src", $(this).find("img").attr("src")).attr("alt", "portrait")
 				$("main>h2 i").css("color", "black")
@@ -55,6 +79,19 @@
 				case "Escape":
 				if($(".overlay").is(":visible")){
 					$(".hampre2").trigger("click")
+					$("html").on("keydown", function(_e){																//call SlideOut() on "esc" press for convenience
+					switch(_e.key){
+						case "Escape":
+						if($(".hampost").is(":visible")){
+							hamslideoff()
+							break
+						}
+						else if($(".hampost").is(":hidden")){
+							hamslidein()
+							break
+						}
+					}
+				})
 					break;
 				}
 			}
